259bdcff4921635874ff1a35a8dc6421e4c9b8ce
Simoffsky <maks-kirsanov26@mail.ru>
2023-07-10 22:58:24 +1000

    removed useless function

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from src.drawer import *
 from src.generator import *
 from src.algo.greedy import *
 
-def color_vertex(vertex, color):
-    vertex.color = color
-g = generate_graph(10)
+g = generate_graph(20)
 
 color_number = greedy(g)
 print("Color number for this graph:", color_number)
